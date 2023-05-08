6fb872047eb2272ca2c500e57b577da8dd766bc1
powerninja <asdyuy23@gmail.com>
2023-05-08 21:46:28 +0900

    9x9のフィールドを表示

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { StrictMode } from 'react';
+import ReactDOM, { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
