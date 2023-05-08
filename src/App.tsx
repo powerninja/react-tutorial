01a71ac0eefb7fa98ee27217e296330b3e0b81fe
powerninja <asdyuy23@gmail.com>
2023-05-09 00:50:13 +0900

    状態の保存を追加したが、想定通り動かず

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 type numValue = {
@@ -8,7 +8,29 @@ type numValue = {
 //<Square value="1" />で関数を呼び出すとprops.valueとして値が渡される
 //そのため、{value}と記載し分割代入をおこなっている
 const Square = ({ value }: numValue) => {
-  return <button className="square">{value}</button>;
+  const [status, setStatus] = useState<string>('');
+  const [checkPlayer, setcheckPlayer] = useState<boolean>(false);
+
+  const aaa = (num: string) => {
+    if (checkPlayer) {
+      setStatus('⭕️');
+      setPlayerFlag(false);
+    } else {
+      setStatus('❌');
+      setPlayerFlag(true);
+    }
+  };
+
+  const setPlayerFlag = (flag: boolean) => {
+    console.log(flag);
+    setcheckPlayer(flag);
+  };
+
+  return (
+    <button className="square" onClick={() => aaa(value)}>
+      {status}
+    </button>
+  );
 };
 
 export const Board = () => {
