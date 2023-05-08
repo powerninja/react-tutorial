aece47916190f730ac0d31dedeb7f48ea466ac48
powerninja <asdyuy23@gmail.com>
2023-05-08 22:04:49 +0900

    メモ追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
 import React from 'react';
 import './App.css';
 
+type numValue = {
+  value: string;
+};
+
+//<Square value="1" />で関数を呼び出すとprops.valueとして値が渡される
+//そのため、{value}と記載し分割代入をおこなっている
+const Square = ({ value }: numValue) => {
+  return <button className="square">{value}</button>;
+};
+
 export const Board = () => {
   return (
     <>
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
       </div>
     </>
   );
