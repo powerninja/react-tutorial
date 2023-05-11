d9162a3199c2a5ed99368662c11711ccc9ac5284
powerninja <asdyuy23@gmail.com>
2023-05-12 00:03:13 +0900

    三目並べ完成！

--- src/App.tsx
+++ src/App.tsx
@@ -16,12 +16,32 @@ const Square = ({ value, onSquareClick }: numValue) => {
   );
 };
 
+const calculateWinner = (squares: any) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+};
+
 export const Board = () => {
   const [xIsNext, setXIsNext] = useState<boolean>(true);
   const [squares, setSquares] = useState<any>(Array(9).fill(null));
 
   const handleClick = (num: number) => {
-    if (squares[num]) {
+    if (squares[num] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = [...squares];
@@ -34,8 +54,16 @@ export const Board = () => {
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   };
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else {
+    status = `Next player: ${xIsNext ? '❌' : '⭕️'}`;
+  }
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
